1eab88552feb5c314523787781a638fe049ba489
medousa89 <medousa89@gmail.com>
2024-08-07 15:50:18 +0300

    Update s1.sh

--- s1.sh
+++ s1.sh
@@ -2,8 +2,8 @@ rm -rf epg.xml.xz
 rm -rf epg.xml
 rm -rf epg1.xml
 wget "https://ext.greektv.app/epg/epg.xml"
-echo "<!--time=` TZ='Europe/Athens' date ` -->" >> epg.xml
 tv_sort --by-channel --duplicate-error --output new.xml epg.xml
+echo "<!-- time=` TZ='Europe/Athens' date ` -->" >> new.xml
 mv new.xml epg.xml
 cp epg.xml epg1.xml
 xz -v epg.xml
